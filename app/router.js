e2280b62cad508969dd2de99fa0c9bf50c10ffd5
Shawn Kebel <fxrender@outlook.com>
2021-11-20 09:07:49 -0600

    :sparkles: GET current-listings

--- app/router.js
+++ app/router.js
@@ -1,10 +1,19 @@
 import Router from "express";
+import client from "./client.js";
+import config from "./config.js";
+
+const collection = client.db(config.db.name).collection(config.db.collection);
 
 const router = new Router();
 
 // GET: /api
 router.get("/", (_, res) => {
   res.send("Hello from API!");
+});
+
+router.get("/current-listings", async (_, res) => {
+  const currentListings = await collection.find({}).limit(5).toArray();
+  res.json(currentListings);
 });
 
 export default router;
