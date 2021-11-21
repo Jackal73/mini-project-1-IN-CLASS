104ea4b99606e5e3461801675d0d9ee26a859bbd
Shawn Kebel <fxrender@outlook.com>
2021-11-21 13:23:44 -0600

    :construction: Query max-price(lte, price) for current-listings

--- app/controller.js
+++ app/controller.js
@@ -4,6 +4,12 @@ import config from "./config.js";
 export default {
   // get all the stuff
   index(queryParams) {
+    // Destructure queryParams, taking out limit, using spread operator, calling rest of params 'filters' .
+    const { limit, ...filters } = queryParams;
+
+    // console.log(limit, filters);
+    // console.log(Object.entries(filters));
+
     return client
       .db(config.db.name)
       .collection(config.db.collection)
@@ -13,5 +19,40 @@ export default {
   },
 };
 
-// Test URLs
-// http://localhost:3000/api/current-listings?limit=1
+// ------------------------------ Self Notes ------------------------------
+// ***--- Test URLs ---***
+// With "limit" = http://localhost:3000/api/current-listings?limit=1
+
+// With "lte" = http://localhost:3000/api/current-listings?limit=1&lte=200
+
+// With "keywords" = http://localhost:3000/api/current-listings?limit=1&lte=200&keywords=sunny+fantastic
+
+// ***--- USE Bracket notation, do NOT use dot notation: will not work! ---***
+// Example:
+
+// const person = {
+//   name: "mark",
+//   property2Add: 23
+// }
+
+// const property2Add = "age"
+// person.name
+// person[property2Add] = 23
+
+// ***--- Array destructuring: review notes(177) ---***
+// Example:
+
+// const groceries = ["apple", "pear", "carrot"]
+// const [a, p] = groceries
+
+// ***--- Doesn't work - mess with later ---***
+// Use reduce and array destructuring.
+// const listingsFilters = Object.entries(filters).reduce(
+//   (accumulator, [keyword, value]) => {
+//     accumulator[keyword] = { [`$${keyword}`]: value };
+//     return accumulator;
+//   },
+//   {}
+// );
+
+// console.log(listingsFilters);
